786a16e42d6d6a81b07fd92da0ad8c7c356bce95
Anna Dybas <a.m.dybas@gmail.com>
2018-04-05 18:46:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,11 +30,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens = kittens.slice(0, kittens.length - 1);
-  return kittens;
+ return kittens = kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten() {
-  kittens = kittens.slice(1);
-  return kittens;
+ return kittens = kittens.slice(1);
 }
